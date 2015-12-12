0c2ea43b7650e8e47403e1f7b9d96573c175b55f
Dylan Wolf <dylan@dylanwolf.com>
2015-12-12 09:19:21 -0500

    Progress

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class CameraScroll : MonoBehaviour {
 
+	Camera cam;
 	Player player;
 	Transform _pt;
 	Transform _t;
+	Rigidbody2D _pr;
+	Rigidbody2D _r;
+
+	float defaultSize;
 
 	public float InitAcceleration;
 
+	public float FailSpeed;
+	public float RecoverySpeed;
+	public float ZoomSpeed;
 	public float PlayerOffset;
 	public float Speed;
 
@@ -16,12 +24,16 @@ public class CameraScroll : MonoBehaviour {
 	{
 		GameState.CurrentPlayState = GameState.PlayState.Init;
 		_t = transform;
+		_r = GetComponent<Rigidbody2D>();
 	}
 
 	void Start()
 	{
+		cam = Camera.main;
+		defaultSize = cam.orthographicSize;
 		player = Player.Current;
 		_pt = player.transform;
+		_pr = player.GetComponent<Rigidbody2D>();
 
 		speed = 0;
 		targetPos.z = _t.position.z;
@@ -29,9 +41,46 @@ public class CameraScroll : MonoBehaviour {
 		targetPos.y = _pt.position.y;
 	}
 
+	float extraSize;
+	void SetTargetSize()
+	{
+		if (Player.Current.CurrentScale >= 1f)
+		{
+			targetSize = 17;
+		}
+		else if (Player.Current.CurrentScale > 0.8f)
+		{
+			targetSize = 15;
+		}
+		else if (Player.Current.CurrentScale > 0.5f)
+		{
+			targetSize = 12;
+		}
+		if (Player.Current.CurrentScale > 0.3f)
+		{
+			targetSize = 9;
+		}
+		else if (Player.Current.CurrentScale > 0.15f)
+		{
+			targetSize = 7;
+		}
+		else
+		{
+			targetSize = 5;
+		}
+	}
+
 	float speed;
     Vector3 targetPos;
 	Vector3 translatedPos;
+	float targetSize;
+	float scale;
+	float newSize;
+	float scaleSpeed;
+
+	float bufferX;
+	float expectedPlayerVelocity;
+
 	void Update()
 	{
 		if (GameState.CurrentPlayState == GameState.PlayState.Init)
@@ -43,20 +92,48 @@ public class CameraScroll : MonoBehaviour {
 			{
 				GameState.CurrentPlayState = GameState.PlayState.Playing;
 				translatedPos = targetPos;
+
+				bufferX = PlayerOffset;
 			}
 
 			_t.position = translatedPos;
 		}
 		else if (GameState.CurrentPlayState == GameState.PlayState.Playing)
 		{
-			targetPos.z = _t.position.z;
-			targetPos.x = _pt.position.x + PlayerOffset;
+			SetTargetSize();
+			scale = targetSize / defaultSize;
+
+			// Scale camera zoom
+			if (cam.orthographicSize < targetSize)
+			{
+				scaleSpeed = Time.deltaTime * GameState.TimeScale * ZoomSpeed;
+				if (scaleSpeed > 1)
+					scaleSpeed = 1;
+				newSize = cam.orthographicSize + ((targetSize - cam.orthographicSize) * scaleSpeed);
+				cam.orthographicSize = newSize;
+			}
+
+			// See if player has been detained
+			expectedPlayerVelocity = (player.CurrentScale * GameState.TimeScale * player.RunSpeed) - 0.1f;
+
+			if (_pr.velocity.x < expectedPlayerVelocity)
+			{
+				bufferX += (expectedPlayerVelocity - _pr.velocity.x) * Time.deltaTime * GameState.TimeScale * FailSpeed;
+			}
+			else
+			{
+				bufferX -= (RecoverySpeed * Time.deltaTime * GameState.TimeScale);
+			}
+
+			if (bufferX < PlayerOffset)
+				bufferX = PlayerOffset;
+
+			// Lock Y position
+			targetPos = _t.position;
+			targetPos.x = _pt.position.x + bufferX;
 			targetPos.y = player.LastGroundedPosition.y;
 
-			if (_t.position.x > targetPos.x)
-				targetPos.x = _t.position.x;
-
-			speed = Time.deltaTime * GameState.TimeScale * Speed;
+			speed = Time.deltaTime * GameState.TimeScale * Speed * scale;
 			if (speed > 1)
 				speed = 1;
 
