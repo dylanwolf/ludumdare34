5597b1158b71ff76feb9c6247940b5628d916ac9
Dylan Wolf <dylan@dylanwolf.com>
2015-12-13 13:03:12 -0500

    First release

---
+++ Assets/Scripts/OffScreenDeath.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OffScreenDeath : MonoBehaviour {
+
+	Camera cam;
+	BoxCollider2D _c;
+
+
+	void Awake()
+	{
+		_c = GetComponent<BoxCollider2D>();
+	}
+	
+	void Start () {
+		cam = Camera.main;
+		UpdateBoundingBox();
+	}
+
+	Vector2 tmp;
+	void UpdateBoundingBox()
+	{
+		tmp = _c.size;
+		tmp.x = cam.aspect * cam.orthographicSize * 2;
+		tmp.y = cam.orthographicSize * 4;
+		_c.size = tmp;
+
+		lastOrthoSize = cam.orthographicSize;
+	}
+
+	float lastOrthoSize;
+
+	void Update()
+	{
+		if (dead)
+		{
+			if (Input.anyKeyDown)
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		else
+		{
+			if (lastOrthoSize != cam.orthographicSize)
+				UpdateBoundingBox();
+		}
+	}
+
+	bool dead = false;
+	const string PLAYER_TAG = "Player";
+	void OnTriggerExit2D(Collider2D c)
+	{
+		if (c.tag == PLAYER_TAG && GameState.CurrentPlayState == GameState.PlayState.Playing)
+		{
+			dead = true;
+		}
+	}
+
+}
