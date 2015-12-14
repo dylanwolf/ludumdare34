46d6eec5282dc81a9676b9b10561f2a50725185c
dylan <dylan@dylanwolf.com>
2015-12-13 19:49:21 -0500

    Final release

---
+++ Assets/Scripts/MessageScreens.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageScreens : MonoBehaviour {
+
+	public SpriteRenderer GameOverScreen;
+	public SpriteRenderer WinnerScreen;
+
+	Transform _got;
+	Transform _wst;
+
+	Vector3 scale;
+
+	float scaleToOrthosize = 1.0f/5.0f;
+
+	Camera cam;
+
+	void Start()
+	{
+		cam = Camera.main;
+		GameState.IsAlive = true;
+		GameState.HasWon = false;
+		_got = GameOverScreen.transform;
+		_wst = WinnerScreen.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameOverScreen.enabled = !GameState.IsAlive;
+		WinnerScreen.enabled = GameState.HasWon;
+
+		if (GameOverScreen.enabled)
+		{
+			scale = _got.localScale;
+			scale.x = scale.y = (cam.orthographicSize * scaleToOrthosize);
+			_got.localScale = scale;
+		}
+
+		if (WinnerScreen.enabled)
+		{
+			scale = _wst.localScale;
+			scale.x = scale.y = (cam.orthographicSize * scaleToOrthosize);
+			_wst.localScale = scale;
+		}
+	}
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	Camera cam;
+	Transform _t;
+	Transform _ct;
+
+	float minCameraX = -29.6f;
+	float MaxCameraX = 405;
+
+	float minImgX = 2.17f;
+	float maxImgX = -1.9f;
+
+	float lastOrthoSize;
+	const float scaleToOrthoSizeRatio = 1.0f / 5.0f;
+
+	void Awake()
+	{
+		_t = transform;
+	}
+
+	void Start () {
+		cam = Camera.main;
+		_ct = cam.transform;
+		minCameraX = _ct.position.x;
+		lastOrthoSize = cam.orthographicSize;
+	}
+
+	float pct;
+	float x;
+	Vector3 pos;
+	Vector3 scale;
+	void Update () {
+		if (lastOrthoSize != cam.orthographicSize)
+		{
+			scale = _t.localScale;
+			scale.x = scale.y = (scaleToOrthoSizeRatio * cam.orthographicSize);
+			_t.localScale = scale;
+			lastOrthoSize = cam.orthographicSize;
+		}
+
+
+		pct = (_ct.position.x - minCameraX) / (MaxCameraX - minCameraX);
+		x = (pct * (maxImgX - minImgX)) + minImgX;
+
+		pos = _t.localPosition;
+		pos.x = x;
+		_t.localPosition = pos;
+	}
+}
